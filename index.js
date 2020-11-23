694fac534dba5149eeab3659688b153eaed3deec
Ian Hutson <ih00209@georgiasouthern.edu>
2020-11-23 17:50:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
-const {name, color, album, job, partner} = muppet
-const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
-const {k_color, k_job, k_partner} = k_muppet
+const {name, color, album, job, partner} = muppet;
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie;
+const {k_color, k_job, k_partner} = k_muppet;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
